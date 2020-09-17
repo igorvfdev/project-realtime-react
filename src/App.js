3e87d6747526b88f2e1adec4ebfc42542708cac4
Igor <igor_vf@outlook.com>
2020-09-17 19:17:33 -0300

    Protótipo

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Header from './components/Header/Header';
+import Table from './components/Table/Table'
+import Footer from './components/Footer/Footer'
+
+const App = () => {
+  return (
+    <div>
+
+    <Header />
+    <Table />
+    <Footer />
+      <div className="App">
+      
+      
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = () =>{
+    return(
+        <div className = 'mainHeader'>
+          <div className = 'logo'>
+            <h1>Header</h1>
+          </div>
+            <div className = 'divisorHeader'>
+
+            </div>
+        </div>
+    )
+
+}
+
+export default Header;
